612f3aacafe2be2e682d7c2964b229102edf1dc7
Ana Ascua <83845164+aniascua@users.noreply.github.com>
2024-06-24 17:07:08 -0300

    Agrego funcionalidades de búsqueda
    Ahora se pueden buscar libros por Título, Género, Autor y Editorial

--- biblioteca/main.py
+++ biblioteca/main.py
@@ -1,7 +1,7 @@
-"""TRABAJO FINAL INTEGRADOR PROGRAMACIÓN 1
-ALUMNA: ASCUA, ANA
-TEMA ELEGIDO: SISTEMA DE GESTIÓN DE BIBLIOTECAS EN PYTHON.
-"""
+# TRABAJO FINAL INTEGRADOR PROGRAMACIÓN 1
+# ALUMNA: ASCUA, ANA
+# TEMA ELEGIDO: SISTEMA DE GESTIÓN DE BIBLIOTECAS EN PYTHON.
+
 # IMPORTAMOS LAS FUNCIONES Y CLASES NECESARIAS
 from utils import cargar_datos, guardar_datos, inicializar_datos  # Funciones para manejar archivos JSON e inicializar datos
 from libro import Libro  # Clase para representar libros
@@ -27,8 +27,12 @@ def mostrar_menu():
     print("8. Devolver Libro")
     print("9. Listar Libros")
     print("10. Listar Socios")
-    print("11. Salir...")
-    print("12. Eliminar Todos los Libros")  # Nueva opción añadida
+    print("11. Buscar Libro por Título")
+    print("12. Buscar Libro por Género")
+    print("13. Buscar Libro por Autor")
+    print("14. Buscar Libro por Editorial")
+    print("15. **EXTRA** Eliminar todos los libros")  # FUNCIONALIDAD EXTRA PARA BORRAR TODOS LOS LIBROS
+    print("16. Salir...")
 
 # 1. Registrar Libro
 def registrar_libro():
@@ -36,15 +40,15 @@ def registrar_libro():
     titulo = input("Título: ")
     autor = input("Autor: ")
     editorial = input("Editorial: ")
-    ano_publicacion = int(input("Año de publicación: "))
+    anio_publicacion = int(input("Año de publicación: "))
     genero = input("Género: ")
     cantidad_disponible = int(input("Cantidad disponible: "))
 
-    libro = Libro(titulo, autor, editorial, ano_publicacion, genero, cantidad_disponible)
+    libro = Libro(titulo, autor, editorial, anio_publicacion, genero, cantidad_disponible)
     
-    libros = cargar_datos('libros.json')
+    libros = cargar_datos(DATA_PATH_LIBROS)
     libros.append(libro.to_dict())
-    guardar_datos('libros.json', libros)
+    guardar_datos(DATA_PATH_LIBROS, libros)
     
     print("Libro registrado exitosamente!")
 
@@ -52,7 +56,7 @@ def registrar_libro():
 def editar_libro():
     """FUNCIÓN PARA EDITAR UN LIBRO EXISTENTE EN EL SISTEMA"""
     id_libro = int(input("ID del libro a editar: "))
-    libros = cargar_datos('libros.json')
+    libros = cargar_datos(DATA_PATH_LIBROS)
     libro_encontrado = False
 
     for libro in libros:
@@ -60,14 +64,14 @@ def editar_libro():
             libro['titulo'] = input(f"Título ({libro['titulo']}): ") or libro['titulo']
             libro['autor'] = input(f"Autor ({libro['autor']}): ") or libro['autor']
             libro['editorial'] = input(f"Editorial ({libro['editorial']}): ") or libro['editorial']
-            libro['año_publicacion'] = int(input(f"Año de publicación ({libro['año_publicacion']}): ") or libro['año_publicacion'])
+            libro['anio_publicacion'] = int(input(f"Año de publicación ({libro['anio_publicacion']}): ") or libro['anio_publicacion'])
             libro['genero'] = input(f"Género ({libro['genero']}): ") or libro['genero']
             libro['cantidad_disponible'] = int(input(f"Cantidad disponible ({libro['cantidad_disponible']}): ") or libro['cantidad_disponible'])
             libro_encontrado = True
             break
 
     if libro_encontrado:
-        guardar_datos('libros.json', libros)
+        guardar_datos(DATA_PATH_LIBROS, libros)
         print("Libro editado exitosamente!")
     else:
         print("Libro no encontrado...")
@@ -76,15 +80,16 @@ def editar_libro():
 def eliminar_libro():
     """FUNCIÓN PARA ELIMINAR UN LIBRO DEL SISTEMA"""
     id_libro = int(input("ID del libro a eliminar: "))
-    libros = cargar_datos('libros.json')
+    libros = cargar_datos(DATA_PATH_LIBROS)
     
     libros = [libro for libro in libros if libro['id_libro'] != id_libro]
     
-    guardar_datos('libros.json', libros)
+    guardar_datos(DATA_PATH_LIBROS, libros)
     print("Libro eliminado exitosamente!")
 
 # 4. Registrar Socio
 def registrar_socio():
+    """FUNCIÓN PARA REGISTRAR UN NUEVO SOCIO EN EL SISTEMA"""
     nombre = input("Nombre: ")
     apellido = input("Apellido: ")
     fecha_nacimiento = input("Fecha de nacimiento (yyyy-mm-dd): ")
@@ -105,13 +110,10 @@ def registrar_socio():
 def editar_socio():
     """FUNCIÓN PARA EDITAR UN SOCIO EXISTENTE EN EL SISTEMA"""
     id_socio = input("ID del socio a editar: ")
-    socios = cargar_datos('socios.json')
-    
-    print("Socios cargados desde el archivo:")
-    print(socios)  # Agregar esta línea para verificar qué socios se están cargando
+    socios = cargar_datos(DATA_PATH_SOCIOS)
     
     socio_encontrado = False
-    # BÚCLE FOR QUE ITERA EN LOS SOCIOS PARA BUSCARLO Y LE PERMITE AL USUARIO EDITAR LOS DATOS PREVIAMENTE INGRESADOS
+
     for socio in socios:
         if socio['id_socio'] == id_socio:
             socio['nombre'] = input(f"Nombre ({socio['nombre']}): ") or socio['nombre']
@@ -124,27 +126,21 @@ def editar_socio():
             break
 
     if socio_encontrado:
-        guardar_datos('socios.json', socios)
+        guardar_datos(DATA_PATH_SOCIOS, socios)
         print("Socio editado exitosamente!")
     else:
         print("Socio no encontrado.")
 
 # 6. Eliminar Socio
 def eliminar_socio():
+    """FUNCIÓN PARA ELIMINAR UN SOCIO DEL SISTEMA"""
     listar_socios()  # Mostrar la lista de socios para que el usuario seleccione uno
     id_socio_a_eliminar = input("Ingrese el ID del socio a eliminar: ")
 
-    # Cargar la lista de socios desde el archivo JSON
-    socios = cargar_datos('socios.json')
-    
-    print("Socios cargados desde el archivo:")
-    print(socios)  # Añadir esta línea para verificar qué socios se están cargando
-    
-    # Filtrar y eliminar el socio por su ID
+    socios = cargar_datos(DATA_PATH_SOCIOS)
     socios_actualizados = [socio for socio in socios if socio['id_socio'] != id_socio_a_eliminar]
     
-    # Guardar los datos actualizados en el archivo JSON
-    guardar_datos('socios.json', socios_actualizados)
+    guardar_datos(DATA_PATH_SOCIOS, socios_actualizados)
     
     print(f"Socio con ID {id_socio_a_eliminar} eliminado correctamente!")
 
@@ -152,22 +148,22 @@ def eliminar_socio():
 def registrar_prestamo():
     """FUNCIÓN PARA REGISTRAR UN NUEVO PRÉSTAMO EN EL SISTEMA"""
     id_libro = int(input("ID del libro: "))
-    id_socio = int(input("ID del socio: "))
+    id_socio = input("ID del socio: ")
     fecha_prestamo = input("Fecha de préstamo (yyyy-mm-dd): ")
 
     prestamo = Prestamo(id_libro, id_socio, fecha_prestamo)
-    prestamos = cargar_datos('prestamos.json')
+    prestamos = cargar_datos(DATA_PATH_PRESTAMOS)
     prestamos.append(prestamo.to_dict())
-    guardar_datos('prestamos.json', prestamos)
+    guardar_datos(DATA_PATH_PRESTAMOS, prestamos)
 
-    libros = cargar_datos('libros.json')
+    libros = cargar_datos(DATA_PATH_LIBROS)
     libro_encontrado = False
 
     for libro in libros:
         if libro['id_libro'] == id_libro:
             if libro['cantidad_disponible'] > 0:
                 libro['cantidad_disponible'] -= 1
-                guardar_datos('libros.json', libros)
+                guardar_datos(DATA_PATH_LIBROS, libros)
                 print("Préstamo registrado exitosamente!")
                 libro_encontrado = True
             else:
@@ -183,35 +179,32 @@ def devolver_libro():
     id_prestamo = input("ID del préstamo: ")
     fecha_devolucion = input("Fecha de devolución (yyyy-mm-dd): ")
 
-    prestamos = cargar_datos('prestamos.json')
+    prestamos = cargar_datos(DATA_PATH_PRESTAMOS)
     prestamo_encontrado = False
     indices_eliminar = []
 
     for index, prestamo in enumerate(prestamos):
-        try:
-            if str(prestamo['id_prestamo']) == id_prestamo:
-                prestamo['fecha_devolucion'] = fecha_devolucion
-                guardar_datos('prestamos.json', prestamos)
-                
-                id_libro = prestamo['id_libro']
-                libros = cargar_datos('libros.json')
-                for libro in libros:
-                    if libro['id_libro'] == id_libro:
-                        libro['cantidad_disponible'] += 1
-                        guardar_datos('libros.json', libros)
-                        break
-                
-                print("Libro devuelto exitosamente.")
-                prestamo_encontrado = True
-                indices_eliminar.append(index)
-                break
-        except KeyError as e:
-            print(f"Error: KeyError - {e}")
+        if str(prestamo['id_prestamo']) == id_prestamo:
+            prestamo['fecha_devolucion'] = fecha_devolucion
+            guardar_datos(DATA_PATH_PRESTAMOS, prestamos)
+            
+            id_libro = prestamo['id_libro']
+            libros = cargar_datos(DATA_PATH_LIBROS)
+            for libro in libros:
+                if libro['id_libro'] == id_libro:
+                    libro['cantidad_disponible'] += 1
+                    guardar_datos(DATA_PATH_LIBROS, libros)
+                    break
+            
+            print("Libro devuelto exitosamente.")
+            prestamo_encontrado = True
+            indices_eliminar.append(index)
+            break
 
     if prestamo_encontrado:
         # Eliminar préstamos marcados para eliminar
         prestamos = [prestamo for index, prestamo in enumerate(prestamos) if index not in indices_eliminar]
-        guardar_datos('prestamos.json', prestamos)
+        guardar_datos(DATA_PATH_PRESTAMOS, prestamos)
 
     if not prestamo_encontrado:
         print("Préstamo no encontrado...")
@@ -219,28 +212,145 @@ def devolver_libro():
 # 9. Listar Libros
 def listar_libros():
     """FUNCIÓN PARA LISTAR TODOS LOS LIBROS DISPONIBLES EN EL SISTEMA"""
-    libros = cargar_datos('libros.json')
+    libros = cargar_datos(DATA_PATH_LIBROS)
     if libros:
         for libro in libros:
-            print(f"ID: {libro['id_libro']}, Título: {libro['titulo']}, Autor: {libro['autor']}, Editorial: {libro['editorial']}, Año: {libro['año_publicacion']}, Género: {libro['genero']}, Disponible: {libro['cantidad_disponible']}")
+            # Asegurarse de que todas las claves necesarias estén presentes en el diccionario del libro
+            id_libro = libro.get('id_libro', 'Desconocido')
+            titulo = libro.get('titulo', 'Desconocido')
+            autor = libro.get('autor', 'Desconocido')
+            editorial = libro.get('editorial', 'Desconocido')
+            anio_publicacion = libro.get('anio_publicacion', 'Desconocido')
+            genero = libro.get('genero', 'Desconocido')
+            cantidad_disponible = libro.get('cantidad_disponible', 'Desconocido')
+
+            print(f"ID: {id_libro}, Título: {titulo}, Autor: {autor}, Editorial: {editorial}, Año: {anio_publicacion}, Género: {genero}, Disponible: {cantidad_disponible}")
     else:
         print("No hay libros registrados...")
 
 # 10. Listar Socios
 def listar_socios():
     """FUNCIÓN PARA LISTAR TODOS LOS SOCIOS REGISTRADOS EN EL SISTEMA"""
-    socios = cargar_datos('socios.json')
+    socios = cargar_datos(DATA_PATH_SOCIOS)
     if socios:
         for socio in socios:
             print(f"ID: {socio['id_socio']}, Nombre: {socio['nombre']}, Apellido: {socio['apellido']}, Fecha de nacimiento: {socio['fecha_nacimiento']}, Dirección: {socio['direccion']}, Correo electrónico: {socio['correo_electronico']}, Teléfono: {socio['telefono']}")
     else:
         print("No hay socios registrados...")
 
-# 11. Eliminar Todos los Libros
+# 11. Buscar Libro por Título
+def buscar_libro_titulo():
+    """FUNCIÓN PARA BUSCAR UN LIBRO POR TÍTULO"""
+    titulo_buscado = input("Ingrese el título del libro a buscar: ").strip().lower()
+
+    libros = cargar_datos(DATA_PATH_LIBROS)
+    libros_encontrados = []
+
+    for libro in libros:
+        if titulo_buscado in libro['titulo'].strip().lower():
+            libros_encontrados.append(libro)
+
+    if libros_encontrados:
+        for libro in libros_encontrados:
+            # Asegurarse de que todas las claves necesarias estén presentes en el diccionario del libro
+            id_libro = libro.get('id_libro', 'Desconocido')
+            titulo = libro.get('titulo', 'Desconocido')
+            autor = libro.get('autor', 'Desconocido')
+            editorial = libro.get('editorial', 'Desconocido')
+            anio_publicacion = libro.get('anio_publicacion', 'Desconocido')
+            genero = libro.get('genero', 'Desconocido')
+            cantidad_disponible = libro.get('cantidad_disponible', 'Desconocido')
+
+            print(f"ID: {id_libro}, Título: {titulo}, Autor: {autor}, Editorial: {editorial}, Año: {anio_publicacion}, Género: {genero}, Disponible: {cantidad_disponible}")
+    else:
+        print(f"No se encontró ningún libro con el título '{titulo_buscado}'.")
+
+# 12. Buscar Libro por Género
+def buscar_libro_genero():
+    """FUNCIÓN PARA BUSCAR LIBROS POR GÉNERO"""
+    genero_buscado = input("Ingrese el género del libro a buscar: ").strip().lower()
+
+    libros = cargar_datos(DATA_PATH_LIBROS)
+    libros_encontrados = []
+
+    for libro in libros:
+        if genero_buscado in libro['genero'].strip().lower():
+            libros_encontrados.append(libro)
+
+    if libros_encontrados:
+        for libro in libros_encontrados:
+            # Asegurarse de que todas las claves necesarias estén presentes en el diccionario del libro
+            id_libro = libro.get('id_libro', 'Desconocido')
+            titulo = libro.get('titulo', 'Desconocido')
+            autor = libro.get('autor', 'Desconocido')
+            editorial = libro.get('editorial', 'Desconocido')
+            anio_publicacion = libro.get('anio_publicacion', 'Desconocido')
+            genero = libro.get('genero', 'Desconocido')
+            cantidad_disponible = libro.get('cantidad_disponible', 'Desconocido')
+
+            print(f"ID: {id_libro}, Título: {titulo}, Autor: {autor}, Editorial: {editorial}, Año: {anio_publicacion}, Género: {genero}, Disponible: {cantidad_disponible}")
+    else:
+        print(f"No se encontraron libros con el género '{genero_buscado}'.")
+
+# 13. Buscar Libro por Autor
+def buscar_libro_autor():
+    """FUNCIÓN PARA BUSCAR LIBROS POR AUTOR"""
+    autor_buscado = input("Ingrese el autor del libro a buscar: ").strip().lower()
+
+    libros = cargar_datos(DATA_PATH_LIBROS)
+    libros_encontrados = []
+
+    for libro in libros:
+        if autor_buscado in libro['autor'].strip().lower():
+            libros_encontrados.append(libro)
+
+    if libros_encontrados:
+        for libro in libros_encontrados:
+            # Asegurarse de que todas las claves necesarias estén presentes en el diccionario del libro
+            id_libro = libro.get('id_libro', 'Desconocido')
+            titulo = libro.get('titulo', 'Desconocido')
+            autor = libro.get('autor', 'Desconocido')
+            editorial = libro.get('editorial', 'Desconocido')
+            anio_publicacion = libro.get('anio_publicacion', 'Desconocido')
+            genero = libro.get('genero', 'Desconocido')
+            cantidad_disponible = libro.get('cantidad_disponible', 'Desconocido')
+
+            print(f"ID: {id_libro}, Título: {titulo}, Autor: {autor}, Editorial: {editorial}, Año: {anio_publicacion}, Género: {genero}, Disponible: {cantidad_disponible}")
+    else:
+        print(f"No se encontraron libros del autor '{autor_buscado}'.")
+
+# 14. Buscar Libro por Editorial
+def buscar_libro_editorial():
+    """FUNCIÓN PARA BUSCAR LIBROS POR EDITORIAL"""
+    editorial_buscada = input("Ingrese la editorial del libro a buscar: ").strip().lower()
+
+    libros = cargar_datos(DATA_PATH_LIBROS)
+    libros_encontrados = []
+
+    for libro in libros:
+        if editorial_buscada in libro['editorial'].strip().lower():
+            libros_encontrados.append(libro)
+
+    if libros_encontrados:
+        for libro in libros_encontrados:
+            # Asegurarse de que todas las claves necesarias estén presentes en el diccionario del libro
+            id_libro = libro.get('id_libro', 'Desconocido')
+            titulo = libro.get('titulo', 'Desconocido')
+            autor = libro.get('autor', 'Desconocido')
+            editorial = libro.get('editorial', 'Desconocido')
+            anio_publicacion = libro.get('anio_publicacion', 'Desconocido')
+            genero = libro.get('genero', 'Desconocido')
+            cantidad_disponible = libro.get('cantidad_disponible', 'Desconocido')
+
+            print(f"ID: {id_libro}, Título: {titulo}, Autor: {autor}, Editorial: {editorial}, Año: {anio_publicacion}, Género: {genero}, Disponible: {cantidad_disponible}")
+    else:
+        print(f"No se encontraron libros de la editorial '{editorial_buscada}'.")
+
+# 15. Eliminar Todos los Libros
 def eliminar_todos_libros():
     """FUNCIÓN PARA ELIMINAR TODOS LOS LIBROS DEL SISTEMA"""
     libros = []
-    guardar_datos('libros.json', libros)
+    guardar_datos(DATA_PATH_LIBROS, libros)
     print("Todos los libros fueron eliminados del sistema...")
 
 # FUNCION PRINCIPAL
@@ -273,12 +383,20 @@ def main():
         elif opcion == '10':
             listar_socios()
         elif opcion == '11':
+            buscar_libro_titulo()
+        elif opcion == '12':
+            buscar_libro_genero()
+        elif opcion == '13':
+            buscar_libro_autor()
+        elif opcion == '14':
+            buscar_libro_editorial()
+        elif opcion == '15':
+            eliminar_todos_libros()
+        elif opcion == '16':
             print("Saliendo del sistema... Gracias!")
             break
-        elif opcion == '12':
-            eliminar_todos_libros()
         else:
-            print("Opción no válida. Intentalo nuevamente.")
+            print("Opción no válida. Inténtalo nuevamente.")
 
 if __name__ == "__main__":
     main()
